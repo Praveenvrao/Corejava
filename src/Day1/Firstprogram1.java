ff336675a0abd7b5dd7a759e6ae3178928a510cc
Praveenvrao <vpraveenrao453@gmail.com>
2025-08-09 19:07:13 +0530

    Day1 programm

---
+++ src/Day1/Firstprogram1.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class Firstprogram1 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("First syntax for new life");
+		System.out.println(10+44+44+33);
+
+	}
+
+}
