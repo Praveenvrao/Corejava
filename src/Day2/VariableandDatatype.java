8f9f948258ef3da8e89fe37f2c201b148f8ac404
Praveenvrao <vpraveenrao453@gmail.com>
2025-08-10 17:28:21 +0530

    Day2 code

---
+++ src/Day2/VariableandDatatype.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class VariableandDatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 100;
+		double d = 4.5;
+		String Name = "John";
+		System.out.println("My name is "+ Name + "And roll number is "+ a);
+
+	}
+
+}
